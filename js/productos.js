db9d9a7520b17c10baac0c875bb23d76434ec8f2
Wanda Scarpato <wandasscarpato@gmail.com>
2021-07-23 15:33:26 -0300

    Agrego: carga de productos desde el JSON (las imagenes aun no están))

---
+++ js/productos.js
@@ -0,0 +1,36 @@
+/*const URLGET = "nombre del archivo json"
+
+$.getJSON(URLGET, function (respuesta, estado) {
+    if(estado === "success"){
+      let misDatos = respuesta;
+      for (const dato of misDatos) {
+        $(".productosLista").append(`<div class="productos">
+                                <img src="${dato.img}" alt="">
+                                <h3 class="quienesSomosSubTituloP">${dato.nombre}</h3>
+                                <p> ${dato.precio}</p>
+                            </div>`)
+                            
+      }  
+    }
+    });*/
+    cargarJSON();
+    function cargarJSON(){
+      fetch('../productos.json')
+        .then(function(res){
+          return res.json();
+        })
+        .then(function(data){
+          let html='';
+          data.forEach(function (productos){
+           html+=`
+           <div class="productos">
+              <img src="../img/producto1.png" alt="Producto">
+               <h3 class="quienesSomosSubTituloP"> ${productos.nombre}
+              de almendras</h3>
+           </div>
+           `;
+          });
+          var prod=document.querySelector("#productosLista");
+          prod.innerHTML=html;
+         })
+}
